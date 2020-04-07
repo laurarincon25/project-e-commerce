c73cc5f561415d6bccdf7c72faf5033a2d48d0e6
Marynel <marynel.peraza@tmconsulting.mx>
2020-04-07 12:49:04 -0400

    modificaciones del crud

--- src/views/container/Articulos/Articulos.vue
+++ src/views/container/Articulos/Articulos.vue
@@ -26,78 +26,315 @@
         style="max-width: 250px;"
       />
 
-      <v-divider class="mt-3" />
+      <v-dialog
+        v-model="dialog"
+        max-width="800px"
+      >
+        <template v-slot:activator="{ on }">
+          <v-btn
+            color="general"
+            dark
+            class="mb-2"
+            v-on="on"
+          >
+            New Articulo
+          </v-btn>
+        </template>
 
+        <v-card>
+          <v-card-text>
+            <v-container>
+              <v-row>
+                <v-col
+                  cols="12"
+                  sm="6"
+                  md="4"
+                >
+                  <v-text-field
+                    v-model="editedItem.name"
+                    label="Nombre"
+                  />
+                </v-col>
+                <v-col
+                  cols="12"
+                  sm="6"
+                  md="4"
+                >
+                  <v-text-field
+                    v-model="editedItem.cant"
+                    label="Cantidad"
+                  />
+                </v-col>
+                <v-col
+                  cols="12"
+                  sm="6"
+                  md="4"
+                >
+                  <v-text-field
+                    v-model="editedItem.peso"
+                    label="Peso (g)"
+                  />
+                </v-col>
+                <v-col
+                  cols="12"
+                  sm="6"
+                  md="4"
+                >
+                  <v-text-field
+                    v-model="editedItem.precio"
+                    label="Precio (Bs)"
+                  />
+                </v-col>
+                <v-col
+                  cols="12"
+                  sm="6"
+                  md="4"
+                >
+                  <v-text-field
+                    v-model="editedItem.desct"
+                    label="Descuento (%)"
+                  />
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-spacer />
+            <v-btn
+              color="blue darken-1"
+              flat
+              @click="close"
+            >
+              Cancel
+            </v-btn>
+            <v-btn
+              color="blue darken-1"
+              flat
+              @click="save"
+            >
+              Save
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+
+      <v-divider class="mt-3" />
       <v-data-table
         :headers="headers"
-        :items="items"
-        :search.sync="search"
-        :sort-by="['name', 'office']"
-        :sort-desc="[false, true]"
-        multi-sort
+        :items="desserts"
         class="elevation-1"
       >
-        <template v-slot:item.actions="{ item }">
-          <v-btn
+        <template
+          slot="headerCell"
+          slot-scope="{ header }"
+        >
+          <span
+            class="subheading font-weight-light text-general text--darken-3"
+            v-text="header.text"
+          />
+        </template>
 
-            :key="1"
-            color="blue"
-            fab
-            class="px-1 ml-1"
-            x-small
-            @click="showArticulos(item)"
-          >
+        <template v-slot:item="props">
+          <td>{{ props.item.name }}</td>
+          <td class="">
+            {{ props.item.cant }}
+          </td>
+          <td class="">
+            {{ props.item.peso }}
+          </td>
+          <td class="">
+            {{ props.item.precio }}
+          </td>
+          <td class="">
+            {{ props.item.desct }}
+          </td>
+          <td class="justify-center ">
             <v-icon
-              small
-              v-text="'mdi-eye'"
-            />
-          </v-btn>
+              medium
+              class="mr-2"
+              @click="editItem(props.item)"
+            >
+              mdi-pencil
+            </v-icon>
+            <!--<v-icon
+              medium
+              @click="deleteItem(props.item)"
+            >
+              restore
+            </v-icon>-->
+            <v-icon
+              medium
+              @click="deleteItem(props.item)"
+            >
+              mdi-delete
+            </v-icon>
+          </td>
+        </template>
+        <template v-slot:no-data>
           <v-btn
-
-            :key="2"
             color="primary"
-            fab
-            class="px-1 ml-1"
-            x-small
-            @click="editArticulos(item)"
+            @click="initialize"
           >
-            <v-icon
-              small
-              v-text="'mdi-pencil'"
-            />
-          </v-btn>
-          <v-btn
-
-            :key="3"
-            color="secondary"
-            fab
-            class="px-1 ml-1"
-            x-small
-            @click="deleteArticulos(item)"
-          >
-            <v-icon
-              small
-              v-text="'mdi-delete'"
-            />
+            Reset
           </v-btn>
         </template>
       </v-data-table>
-      <v-card-text style="height: 100px; position: relative">
-        <v-fab-transition>
-          <v-btn
-            fab
-            dark
-            large
-            color="primary"
-            fixed
-            right
-            bottom
-            @click="createArticulos"
-          >
-            <v-icon>mdi-plus</v-icon>
-          </v-btn>
-        </v-fab-transition>
-      </v-card-text>
     </base-material-card>
   </v-container>
 </template>
+
+<script>
+  export default {
+    data: () => ({
+      dialog: false,
+      headers: [
+        {
+          text: 'Nombre',
+          align: 'left',
+          sortable: false,
+          value: 'name',
+        },
+        { text: 'Cantidad',
+          value: 'cant',
+        },
+        { text: 'Peso (g)',
+          value: 'peso',
+        },
+        { text: 'Precio (Bs)',
+          value: 'precio',
+        },
+        { text: 'Descuento (%)',
+          value: 'desct',
+        },
+        { text: 'Actions',
+          value: 'name',
+          sortable: false,
+        },
+      ],
+      desserts: [],
+      editedIndex: -1,
+      editedItem: {
+        name: '',
+        cant: 0,
+        peso: 0,
+        precio: 0,
+        desct: 0,
+      },
+      defaultItem: {
+        name: '',
+        cant: 0,
+        peso: 0,
+        precio: 0,
+        desct: 0,
+      },
+    }),
+
+    computed: {
+      formTitle () {
+        return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
+      },
+    },
+
+    watch: {
+      dialog (val) {
+        val || this.close()
+      },
+    },
+
+    created () {
+      this.initialize()
+    },
+
+    methods: {
+      initialize () {
+        this.desserts = [
+          {
+            name: 'Jabon',
+            cant: 10,
+            peso: 5,
+            precio: 24000,
+            desct: 0.1,
+          },
+          {
+            name: 'Crema Dental',
+            cant: 5,
+            peso: 1,
+            precio: 1000,
+            desct: 0.05,
+          },
+          {
+            name: 'Mayonesa',
+            cant: 2,
+            peso: 1,
+            precio: 6000,
+            desct: 0.1,
+          },
+          {
+            name: 'Pasta',
+            cant: 25,
+            peso: 40,
+            precio: 55000,
+            desct: 0.15,
+          },
+          {
+            name: 'Arroz',
+            cant: 3,
+            peso: 8,
+            precio: 10500,
+            desct: 0.2,
+          },
+          {
+            name: 'Maiz',
+            cant: 2,
+            peso: 4,
+            precio: 2400,
+            desct: 0.05,
+          },
+          {
+            name: 'Caraotas',
+            cant: 10,
+            peso: 5,
+            precio: 240000,
+            desct: 0.1,
+          },
+          {
+            name: 'Cera',
+            cant: 1,
+            peso: 1,
+            precio: 240,
+            desct: 0.1,
+          },
+        ]
+      },
+
+      editItem (item) {
+        this.editedIndex = this.desserts.indexOf(item)
+        this.editedItem = Object.assign({}, item)
+        this.dialog = true
+      },
+
+      deleteItem (item) {
+        const index = this.desserts.indexOf(item)
+        confirm('Are you sure you want to delete this item?') && this.desserts.splice(index, 1)
+      },
+
+      close () {
+        this.dialog = false
+        setTimeout(() => {
+          this.editedItem = Object.assign({}, this.defaultItem)
+          this.editedIndex = -1
+        }, 300)
+      },
+
+      save () {
+        if (this.editedIndex > -1) {
+          Object.assign(this.desserts[this.editedIndex], this.editedItem)
+        } else {
+          this.desserts.push(this.editedItem)
+        }
+        this.close()
+      },
+    },
+  }
+</script>
--- src/views/container/Articulos/ArticulosForm.vue
+++ src/views/container/Articulos/ArticulosForm.vue
@@ -1,1 +1,285 @@
+<template>
+  <v-container
+    id="articulo-profile"
+    fluid
+    tag="section"
+  >
+    <v-row justify="center">
+      <base-material-card icon="mdi-account-outline">
+        <template v-slot:heading>
+          <v-tabs
+            v-model="tabs"
+            background-color="transparent"
+            slider-color="white"
+          >
+            <v-tab class="mr-3">
+              <v-icon class="mr-2">
+                mdi-account-key
+              </v-icon>
+              <!--{{ getTitleButton }}-->
+              <h1>Articulos</h1>&nbsp;
+            </v-tab>
+          </v-tabs>
+        </template>
 
+        <v-card-text style="height: 100px; position: relative">
+          <v-fab-transition>
+            <v-btn
+              fab
+              dark
+              small
+              color="secondary"
+              absolute
+              right
+              top
+              @click="$router.go(-1)"
+            >
+              <v-icon>mdi-arrow-left</v-icon>
+            </v-btn>
+          </v-fab-transition>
+        </v-card-text>
+
+        <v-data-table
+          :headers="headers"
+          :items="desserts"
+          class="elevation-1"
+        >
+          <!-- change table header color(or other properties) -->
+          <template
+            slot="headerCell"
+            slot-scope="{ header }"
+          >
+            <span
+              class="subheading font-weight-light text-general text--darken-3"
+              v-text="header.text"
+            />
+          </template>
+          <!-- inline editing data table -->
+          <template v-slot:items="props">
+            <td>
+              <v-edit-dialog
+                :return-value.sync="props.item.name"
+                lazy
+                @save="saveInline"
+                @cancel="cancelInline"
+                @open="openInline"
+                @close="closeInline"
+              >
+                {{ props.item.name }}
+                <template v-slot:input>
+                  <v-text-field
+                    v-model="props.item.name"
+                    :rules="[max25chars]"
+                    label="Edit"
+                    single-line
+                    counter
+                  />
+                </template>
+              </v-edit-dialog>
+            </td>
+            <td class="t">
+              {{ props.item.cant }}
+            </td>
+            <td class="text-right">
+              <v-edit-dialog
+                :return-value.sync="props.item.peso"
+                large
+                lazy
+                persistent
+                @save="saveInline"
+                @cancel="cancelInline"
+                @open="openInline"
+                @close="closeInline"
+              >
+                <div>{{ props.item.peso }}</div>
+                <template v-slot:input>
+                  <div class="mt-3 title">
+                    Update Peso
+                  </div>
+                </template>
+                <template v-slot:input>
+                  <v-text-field
+                    v-model="props.item.peso"
+                    :rules="[max25chars]"
+                    label="Edit"
+                    single-line
+                    counter
+                    autofocus
+                  />
+                </template>
+              </v-edit-dialog>
+            </td>
+            <td class="">
+              {{ props.item.precio }}
+            </td>
+            <td class="text-right">
+              <v-edit-dialog
+                :return-value.sync="props.item.desct"
+                large
+                lazy
+                persistent
+                @save="saveInline"
+                @cancel="cancelInline"
+                @open="openInline"
+                @close="closeInline"
+              >
+                <div>{{ props.item.desct }}</div>
+                <template v-slot:input>
+                  <div class="mt-3 title">
+                    Update Descuento
+                  </div>
+                </template>
+                <template v-slot:input>
+                  <v-text-field
+                    v-model="props.item.desct"
+                    :rules="[max25chars]"
+                    label="Edit"
+                    single-line
+                    counter
+                    autofocus
+                  />
+                </template>
+              </v-edit-dialog>
+            </td>
+          </template>
+        </v-data-table>
+        <v-snackbar
+          v-model="snack"
+          :timeout="3000"
+          :color="snackColor"
+        >
+          {{ snackText }}
+          <v-btn
+            flat
+            @click="snack = false"
+          >
+            Close
+          </v-btn>
+        </v-snackbar>
+      </base-material-card>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        snack: false,
+        snackColor: '',
+        snackText: '',
+        max25chars: v => v.length <= 25 || 'Input too long!',
+        pagination: {},
+        headers: [
+          {
+            text: 'Nombre',
+            align: 'left',
+            sortable: false,
+            value: 'name',
+          },
+          { text: 'Cantidad',
+            value: 'cant',
+          },
+          { text: 'Peso (g)',
+            value: 'peso',
+          },
+          { text: 'Precio (Bs)',
+            value: 'precio',
+          },
+          { text: 'Descuento (%)',
+            value: 'desct',
+          },
+          { text: 'Iron (%)',
+            value: 'iron',
+          },
+        ],
+        desserts: [
+          {
+            name: 'Jabon',
+            cant: 10,
+            peso: 5,
+            precio: 24000,
+            desct: 0.1,
+            iron: '1%',
+          },
+          {
+            name: 'Crema Dental',
+            cant: 5,
+            peso: 1,
+            precio: 1000,
+            desct: 0.05,
+            iron: '1%',
+          },
+          {
+            name: 'Mayonesa',
+            cant: 2,
+            peso: 1,
+            precio: 6000,
+            desct: 0.1,
+            iron: '7%',
+          },
+          {
+            name: 'Pasta',
+            cant: 25,
+            peso: 40,
+            precio: 55000,
+            desct: 0.15,
+            iron: '8%',
+          },
+          {
+            name: 'Arroz',
+            cant: 3,
+            peso: 8,
+            precio: 10500,
+            desct: 0.2,
+            iron: '16%',
+          },
+          {
+            name: 'Maiz',
+            cant: 2,
+            peso: 4,
+            precio: 2400,
+            desct: 0.05,
+            iron: '0%',
+          },
+          {
+            name: 'Caraotas',
+            cant: 10,
+            peso: 5,
+            precio: 240000,
+            desct: 0.1,
+            iron: '2%',
+          },
+          {
+            name: 'Cera',
+            cant: 1,
+            peso: 1,
+            precio: 240,
+            desct: 0.1,
+            iron: '45%',
+          },
+        ],
+      }
+    },
+    methods: {
+      save () {
+        this.snack = true
+        this.snackColor = 'success'
+        this.snackText = 'Data saved'
+      },
+      cancel () {
+        this.snack = true
+        this.snackColor = 'error'
+        this.snackText = 'Canceled'
+      },
+      open () {
+        this.snack = true
+        this.snackColor = 'info'
+        this.snackText = 'Dialog opened'
+      },
+      close () {
+        console.log('Dialog closed')
+      },
+    },
+  }
+</script>
